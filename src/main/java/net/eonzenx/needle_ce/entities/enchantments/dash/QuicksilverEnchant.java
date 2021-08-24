72c5e6ad5f13caf58a800d72fbdb12cf3b0a77dc
EonZeNx <sparck.va@gmail.com>
2021-08-24 18:22:39 +1200

    Some Enchantments now block

--- src/main/java/net/eonzenx/needle_ce/entities/enchantments/dash/QuicksilverEnchant.java
+++ src/main/java/net/eonzenx/needle_ce/entities/enchantments/dash/QuicksilverEnchant.java
@@ -1,9 +1,13 @@
 package net.eonzenx.needle_ce.entities.enchantments.dash;
 
+import net.eonzenx.needle_ce.registry_handlers.EnchantmentRegistryHandler;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuicksilverEnchant extends Enchantment {
     public QuicksilverEnchant() {
@@ -17,7 +21,14 @@ public class QuicksilverEnchant extends Enchantment {
     public int getMaxLevel() { return 3; }
 
     @Override
-    protected boolean canAccept(Enchantment other) { return super.canAccept(other); }
+    protected boolean canAccept(Enchantment other) {
+        var blocked = new ArrayList<>(Arrays.asList(
+                EnchantmentRegistryHandler.VAULTING,
+                this
+        ));
+
+        return !blocked.contains(other);
+    }
 
     @Override
     public boolean isAvailableForEnchantedBookOffer() { return true; }
--- src/main/java/net/eonzenx/needle_ce/entities/enchantments/dash/VaultingEnchant.java
+++ src/main/java/net/eonzenx/needle_ce/entities/enchantments/dash/VaultingEnchant.java
@@ -1,5 +1,6 @@
 package net.eonzenx.needle_ce.entities.enchantments.dash;
 
+import net.eonzenx.needle_ce.registry_handlers.EnchantmentRegistryHandler;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
@@ -7,9 +8,12 @@ import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class VaultingEnchant extends Enchantment {
     public VaultingEnchant() {
-        super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.ARMOR_LEGS, new EquipmentSlot[] {EquipmentSlot.LEGS});
+        super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.ARMOR_FEET, new EquipmentSlot[] {EquipmentSlot.FEET});
     }
 
     @Override
@@ -19,7 +23,14 @@ public class VaultingEnchant extends Enchantment {
     public int getMaxLevel() { return 3; }
 
     @Override
-    protected boolean canAccept(Enchantment other) { return super.canAccept(other); }
+    protected boolean canAccept(Enchantment other) {
+        var blocked = new ArrayList<>(Arrays.asList(
+                EnchantmentRegistryHandler.QUICKSILVER,
+                this
+        ));
+
+        return !blocked.contains(other);
+    }
 
     @Override
     public boolean isAvailableForEnchantedBookOffer() { return true; }
